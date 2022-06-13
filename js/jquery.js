9d352bdaa7f41fee71ae94571373b07b250d3c2c
Angell022 <jass461489@gmail.com>
2022-06-12 20:53:30 -0500

    portafolio

--- js/jquery.js
+++ js/jquery.js
@@ -4,23 +4,20 @@ const showMenu = (toggleId, navId) => {
 
   if (toggle && nav) {
     toggle.addEventListener("click", () => {
-     
       nav.classList.toggle("show-menu");
     });
   }
 };
 showMenu("nav-toggle", "nav-menu");
 
-
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  
+
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
-
 
 const sections = document.querySelectorAll("section[id]");
 
@@ -45,19 +42,17 @@ function scrollActive() {
 }
 window.addEventListener("scroll", scrollActive);
 
-
 function scrollHeader() {
   const nav = document.getElementById("header");
-  
+
   if (this.scrollY >= 200) nav.classList.add("scroll-header");
   else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
 
-
 function scrollTop() {
   const scrollTop = document.getElementById("scroll-top");
-  
+
   if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
   else scrollTop.classList.remove("show-scroll");
 }
